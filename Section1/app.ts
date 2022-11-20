30dbdf9e1393fbbb8a89d4940f99534c27690c6d
Beanxx <tnqls991211@naver.com>
2022-11-20 23:23:00 +0900

    feat: 숫자, 문자열, 불리언 타입 학습

--- Section1/app.ts
+++ Section1/app.ts
@@ -1,1 +1,16 @@
-console.log('Your code goes here...');
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result); // Result is: 7.8 (숫자끼리의 계산은 result에서 한 후에 출력하는 것이므로 원하는대로 출력~!)
+    console.log(phrase + n1 + n2); // Result is: 52.8  (문자열 + 숫자 + 숫자 => 문자열 계산)
+  } else {
+    return n1 + n2;
+  }
+}
+
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const resultPhrase = 'Result is: ';
+
+add(number1, number2, printResult, resultPhrase);
